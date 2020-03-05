05e62b87262586000b1a9f3adaf322a8e2f10201
Yaroslav Antipov <yaroslav.antipov@cengage.com>
2020-03-05 18:55:01 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Sky from "react-sky";
 
-function App() {
+import "./App.css";
+
+const App = () => {
+  const randCardIndex = Math.floor(Math.random() * 10) + 1;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="card-container">
+        <img className="card" src={`/images/card${randCardIndex}.jpeg`} alt="Pretty card." />
+      </div>
+      <Sky
+        images={{
+          0: "/images/daisy1.svg",
+          1: "/images/daisy2.svg",
+          2: "/images/daisy3.svg"
+        }}  
+        how={100}
+        time={50}
+        background={"pink"}
+      />
     </div>
   );
-}
+};
 
 export default App;
