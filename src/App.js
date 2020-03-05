bd4de3ff6b7a4cd0325e10791b0a1210ca339770
Yaroslav Antipov <yaroslav.antipov@cengage.com>
2020-03-05 22:45:14 +0200

    Add lyrics.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Sky from "react-sky";
-
+import lyrics from "./lyrics.json";
 import "./App.css";
 
 const App = () => {
@@ -9,18 +9,34 @@ const App = () => {
   return (
     <div>
       <div className="card-container">
-        <img className="card" src={`/images/card${randCardIndex}.jpeg`} alt="Pretty card." />
+        <img
+          className="image-card"
+          src={`/images/card${randCardIndex}.jpeg`}
+          alt="Pretty card."
+        />
       </div>
-      <Sky
-        images={{
-          0: "/images/daisy1.svg",
-          1: "/images/daisy2.svg",
-          2: "/images/daisy3.svg"
-        }}  
-        how={100}
-        time={50}
-        background={"pink"}
-      />
+      <div className="card-container">
+        <div className="lyrics-card">{lyrics[randCardIndex - 1]}</div>
+      </div>
+      <div className="card-container">
+        <div className="final-card">
+          {"   There is\n   only one\nin the world \n   like   "}
+          <span className="fancy-font">you</span>
+        </div>
+      </div>
+      <div className="background-container">
+        <Sky
+          images={{
+            0: "/images/daisy1.svg",
+            1: "/images/daisy2.svg",
+            2: "/images/daisy3.svg"
+          }}
+          how={400}
+          time={50}
+          size={"80px"}
+          background={"pink"}
+        />
+      </div>
     </div>
   );
 };
